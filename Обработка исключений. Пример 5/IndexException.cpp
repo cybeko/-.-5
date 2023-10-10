f4a648a7a98f32fbde4cb94765c7f3b9c8b2161e
Anna <jarviscsp@gmail.com>
2023-10-10 10:34:27 +0300

    Add project files.

---
+++ Обработка исключений. Пример 5/IndexException.cpp
@@ -0,0 +1,8 @@
+#include "IndexException.h"
+
+IndexException::IndexException(const char* mes) : Exception(mes){}
+
+const char* IndexException::GetMessage() const
+{
+	return m_message;
+}
---
+++ Обработка исключений. Пример 5/IndexException.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Exception.h"
+class IndexException : public Exception
+{
+public:
+	IndexException(const char* message);
+	const char* GetMessage() const;
+};
